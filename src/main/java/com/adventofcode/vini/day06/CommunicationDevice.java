2339fb757dde2f703f3df960583ac9357182461a
Ashish <agnihotri.ashish@gmail.com>
2022-12-08 02:35:36 -0800

    Day 06 Puzzles 01 and 02

---
+++ src/main/java/com/adventofcode/vini/day06/CommunicationDevice.java
@@ -0,0 +1,74 @@
+package com.adventofcode.vini.day06;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CommunicationDevice {
+    String dataStream;
+
+    public CommunicationDevice(String dataStream) {
+        this.dataStream = dataStream;
+    }
+
+    public String getDataStream() {
+        return this.dataStream;
+    }
+
+    public int getStartOfPacketMarker() {
+        char[] dataArr = this.dataStream.toCharArray();
+
+        if(this.dataStream == null || this.dataStream.isEmpty() || this.dataStream.length() < 3) {
+            return -1;
+        }
+
+        int uniqueCharCount = 0;
+        for(int i=0; i<dataArr.length - 4; i++) {
+            Set<Character> set = new LinkedHashSet<>();
+            uniqueCharCount = 0;
+            
+            for(int j=i; j<i+4; j++) {
+                if(!set.add(dataArr[j])) {
+                    break;
+                } else {
+                    uniqueCharCount++;
+                    if(uniqueCharCount == 4) {
+                        System.out.println(set);
+                        return j + 1; // Return 'position' not 'index'
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int getStartOfMessageMarker() {
+        char[] dataArr = this.dataStream.toCharArray();
+
+        if(this.dataStream == null || this.dataStream.isEmpty() || this.dataStream.length() < 3) {
+            return -1;
+        }
+
+        int uniqueCharCount = 0;
+        for(int i=0; i<dataArr.length - 4; i++) {
+            Set<Character> set = new LinkedHashSet<>();
+            uniqueCharCount = 0;
+            
+            for(int j=i; j<i+14; j++) {
+                if(!set.add(dataArr[j])) {
+                    break;
+                } else {
+                    uniqueCharCount++;
+                    if(uniqueCharCount == 14) {
+                        System.out.println(set);
+                        return j + 1; // Return 'position' not 'index'
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/main/java/com/adventofcode/vini/day06/Puzzle01.java
@@ -0,0 +1,15 @@
+package com.adventofcode.vini.day06;
+
+import java.util.List;
+
+import com.adventofcode.vini.util.FileReader;
+
+public class Puzzle01 {
+    public static void main(String[] args) {
+        List<String> lst = FileReader.allLines("day06/input.txt");
+        String dataStream = lst.get(0);
+        CommunicationDevice device = new CommunicationDevice(dataStream);
+        System.out.println(device.getStartOfPacketMarker());
+        System.out.println(device.getStartOfMessageMarker());
+    }
+}
