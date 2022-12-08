288cde7e92f72e540391241c601f105008ab16e6
Ashish <agnihotri.ashish@gmail.com>
2022-12-08 00:43:42 -0800

    Day 5 Puzzle 1

---
+++ src/main/java/com/adventofcode/vini/day05/Puzzle01.java
@@ -0,0 +1,137 @@
+package com.adventofcode.vini.day05;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import com.adventofcode.vini.util.FileReader;
+
+public class Puzzle01 {
+
+    private static Stack<Character> stack1 = new Stack<>();
+    private static Stack<Character> stack2 = new Stack<>();
+    private static Stack<Character> stack3 = new Stack<>();
+    private static Stack<Character> stack4 = new Stack<>();
+    private static Stack<Character> stack5 = new Stack<>();
+    private static Stack<Character> stack6 = new Stack<>();
+    private static Stack<Character> stack7 = new Stack<>();
+    private static Stack<Character> stack8 = new Stack<>();
+    private static Stack<Character> stack9 = new Stack<>();
+
+    private static Map<Integer, Stack<Character>> stackMap = new HashMap<>();
+
+
+    static {
+        initStack(stack1, "NSDCVQT");
+        initStack(stack2, "MFV");
+        initStack(stack3, "FQWDPNHM");
+        initStack(stack4, "DQRTF");
+        initStack(stack5, "RFMNQHVB");
+        initStack(stack6, "CFGNPWQ");
+        initStack(stack7, "WFRLCT");
+        initStack(stack8, "TZNS");
+        initStack(stack9, "MSDJRQHN");
+
+        stackMap.put(1, stack1);
+        stackMap.put(2, stack2);
+        stackMap.put(3, stack3);
+        stackMap.put(4, stack4);
+        stackMap.put(5, stack5);
+        stackMap.put(6, stack6);
+        stackMap.put(7, stack7);
+        stackMap.put(8, stack8);
+        stackMap.put(9, stack9);
+    }
+
+    public static void main(String[] args) {
+        List<String> allLines = FileReader.allLinesFrom("day05/input.txt", 11);
+
+        for(String command: allLines) {
+            Instruction instruction = getInstructionFromCommand(command);
+            processInstruction(instruction);
+        }
+
+        System.out.println(getCratesFromTopLayer());
+
+        
+    }
+
+    private static void initStack(Stack<Character> stack, String crates) {
+        char[] cratesArr = crates.toCharArray();
+        for(Character c: cratesArr) {
+            stack.push(c);
+        }
+    }
+
+    private static String getCratesFromTopLayer() {
+        char[] arr = new char[9];
+        
+        for(int i=0; i<9; i++) {
+            arr[i] = stackMap.get(i+1).pop(); // Stack map is '1' based index
+        }
+
+        return new String(arr);
+    }
+
+    private static void processInstruction(Instruction instruction) {
+        Stack<Character> stackFrom = stackMap.get(instruction.getStackFrom());
+        Stack<Character> stackTo   = stackMap.get(instruction.getStackTo());
+        
+        for(int i=0; i<instruction.getStackCount(); i++) {
+            Character temp = stackFrom.pop();
+            stackTo.push(temp);
+        }
+
+    }
+
+    private static Instruction getInstructionFromCommand(String commandLine) {
+        // Template "move 1 from 8 to 7"
+        String[] parts = commandLine.split(" ");
+        Integer count = Integer.parseInt(parts[1]);
+        Integer fromStack = Integer.parseInt(parts[3]);
+        Integer toStack = Integer.parseInt(parts[5]);
+
+        return new Instruction(count, fromStack, toStack);
+    }
+
+
+}
+
+class Instruction {
+    private Integer stackCount;
+    private Integer stackFrom;
+    private Integer stackTo;
+    
+    public Integer getStackCount() {
+        return stackCount;
+    }
+    
+    public void setStackCount(Integer stackCount) {
+        this.stackCount = stackCount;
+    }
+    
+    public Integer getStackFrom() {
+        return stackFrom;
+    }
+    
+    public void setStackFrom(Integer stackFrom) {
+        this.stackFrom = stackFrom;
+    }
+    
+    public Integer getStackTo() {
+        return stackTo;
+    }
+    
+    public void setStackTo(Integer stackTo) {
+        this.stackTo = stackTo;
+    }
+
+    public Instruction(Integer stackCount, Integer stackFrom, Integer stackTo) {
+        this.stackCount = stackCount;
+        this.stackFrom = stackFrom;
+        this.stackTo = stackTo;
+    }
+
+}
--- src/main/java/com/adventofcode/vini/util/FileReader.java
+++ src/main/java/com/adventofcode/vini/util/FileReader.java
@@ -17,4 +17,22 @@ public class FileReader {
         scanner.close();
         return fileLines;
     }
+
+    public static List<String> allLinesFrom(String fileName, int startingLine) {
+        int currentLine = 0;
+        List<String> fileLines = new ArrayList<>();
+
+        InputStream is = FileReader.class.getClassLoader().getResourceAsStream(fileName);
+        Scanner scanner = new Scanner(is);
+        while(scanner.hasNextLine()) {
+            currentLine++;
+            if(currentLine < startingLine) {
+                scanner.nextLine();
+                continue;
+            }
+            fileLines.add(scanner.nextLine());
+        }
+        scanner.close();
+        return fileLines;
+    }
 }
